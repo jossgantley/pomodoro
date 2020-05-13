9d0f2586a94a5d419430b4e0ae77a4e50d30e55f
josscg449 <38119088+josscg449@users.noreply.github.com>
2020-05-13 04:18:14 -0600

    Updates

---
+++ precache-manifest.a9d30c59dab74396a0e60b966170c396.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18b4765a002afa762a653685efd98e0",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "2bf0b608f04a0fa9b12d",
+    "url": "/pomodoro/static/css/main.772f4f97.chunk.css"
+  },
+  {
+    "revision": "7fbda0079b1d8264377f",
+    "url": "/pomodoro/static/js/2.4b1df254.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro/static/js/2.4b1df254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bf0b608f04a0fa9b12d",
+    "url": "/pomodoro/static/js/main.7169b36a.chunk.js"
+  },
+  {
+    "revision": "c45db777bf26ff99c757",
+    "url": "/pomodoro/static/js/runtime-main.b80f58c6.js"
+  },
+  {
+    "revision": "fa54b0dc74775003cdbbfb2eeabb7a84",
+    "url": "/pomodoro/static/media/alarm.fa54b0dc.mp3"
+  }
+]);
